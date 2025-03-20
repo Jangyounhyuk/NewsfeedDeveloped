e690602efb8ebd9cb0686d87f3d8b792345b03ca
JangYounhyuk <hyuk905@gmail.com>
2025-03-20 20:30:42 +0900

    refactor

--- src/test/java/com/example/developednewsfeed/auth/controller/AuthControllerTest.java
+++ src/test/java/com/example/developednewsfeed/auth/controller/AuthControllerTest.java
@@ -11,7 +11,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -20,9 +19,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.mockito.BDDMockito.given;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @WebMvcTest(AuthController.class)
 class AuthControllerTest {
